193253e22f4a0eeabccb59f41ff01a1c2ff858d6
hanaeraza <marine.raza1@gmail.com>
2022-12-08 01:50:40 -0500

    student grade statistics

--- student_grades.php
+++ student_grades.php
@@ -34,10 +34,16 @@
     <br>
 
     <?php
-    $servername = "sql311.epizy.com";
-    $username = "epiz_33100670";
-    $password = "kgKnMKhpvKi";
-    $dbname = "epiz_33100670_stoodle";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "test";
+    
+    $deliv1 = array();
+    $deliv2 = array();
+    $deliv3 = array();
+    $midExam = array();
+    $finExam = array();
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -59,7 +65,7 @@
         echo "<th> Deliverable #2 </th>";
         echo "<th> Deliverable #3 </th>";
         echo "<th> Midterm </th>";
-        echo "<th> Final </th>"
+        echo "<th> Final </th>";
         echo "</tr>";
 
 
@@ -67,10 +73,15 @@
             echo "<tr>";
             echo "<td>" . $row["studentID"] . "</td>";
             echo "<td>" . $row["del1"] . "</td>";
+            $deliv1[] = $row["del1"];
             echo "<td>" . $row["del2"] . "</td>";
+            $deliv2[] = $row["del2"];
             echo "<td>" . $row["del3"] . "</td>";
+            $deliv3[] = $row["del3"];
             echo "<td>" . $row["midterm"] . "</td>";
+            $midExam[] = $row["midterm"];
             echo "<td>" . $row["final"] . "</td>";
+            $finExam[] = $row["final"];
             echo "</tr>";
         }
         echo "</table>";
@@ -78,10 +89,112 @@
         echo "0 results";
     }
 
+        echo"<br>";
+    echo "<table>";
+    echo "<caption>Student Statistics</caption>";
+    echo "<tr>";
+    echo "<th> </th>";
+    echo "<th> Average </th>";
+    echo "<th> Median </th>";
+    echo "<th> Standard Deviation </th>";
+    echo "</tr>";
+
+    $del1_total = array_sum($deliv1);
+    $del2_total = array_sum($deliv2);
+    $del3_total = array_sum($deliv3);
+    $midExam_total = array_sum($midExam);
+    $finExam_total = array_sum($finExam);
+
+    sort($deliv1);
+    sort($deliv2);
+    sort($deliv3);
+    sort($midExam);
+    sort($finExam);
+
+    $length = count($deliv1);
+    $second_half_length = $length / 2;
+    $first_half_length = $second_half_length - 1;
+    $first_half = $deliv1[$first_half_length];
+    $second_half = $deliv1[$second_half_length];
+    $median_del1 = ($first_half + $second_half) / 2;
+
+    $length = count($deliv2);
+    $second_half_length = $length / 2;
+    $first_half_length = $second_half_length - 1;
+    $first_half = $deliv2[$first_half_length];
+    $second_half = $deliv2[$second_half_length];
+    $median_del2 = ($first_half + $second_half) / 2;
+
+    $length = count($deliv3);
+    $second_half_length = $length / 2;
+    $first_half_length = $second_half_length - 1;
+    $first_half = $deliv3[$first_half_length];
+    $second_half = $deliv3[$second_half_length];
+    $median_del3 = ($first_half + $second_half) / 2;
+
+    $length = count($midExam);
+    $second_half_length = $length / 2;
+    $first_half_length = $second_half_length - 1;
+    $first_half = $midExam[$first_half_length];
+    $second_half = $midExam[$second_half_length];
+    $median_midterm = ($first_half + $second_half) / 2;
+
+    $length = count($finExam);
+    $second_half_length = $length / 2;
+    $first_half_length = $second_half_length - 1;
+    $first_half = $finExam[$first_half_length];
+    $second_half = $finExam[$second_half_length];
+    $median_final = ($first_half + $second_half) / 2;
+
+    function std_deviation($arr){
+        $arr_size=count($arr);
+        $mu=array_sum($arr)/$arr_size;
+        $ans=0;
+        foreach($arr as $elem){
+            $ans+=pow(($elem-$mu),2);
+        }
+        return sqrt($ans/$arr_size);
+    }
+
+    echo "<tr>";
+    echo "<td>Deliverable #1</td>";
+    echo "<td>" . ($del1_total/count($deliv1)) . "</td>";
+    echo "<td>" . $median_del1 . "</td>";
+    echo "<td>" . std_deviation($deliv1) . "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>Deliverable #2</td>";
+    echo "<td>" . ($del2_total/count($deliv2)) . "</td>";
+    echo "<td>" . $median_del2 . "</td>";
+    echo "<td>" . std_deviation($deliv2) . "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>Deliverable #3</td>";
+    echo "<td>" . ($del3_total/count($deliv3)) . "</td>";
+    echo "<td>" . $median_del3 . "</td>";
+    echo "<td>" . std_deviation($deliv3) . "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>Midterm</td>";
+    echo "<td>" . ($midExam_total/count($midExam)) . "</td>";
+    echo "<td>" . $median_midterm . "</td>";
+    echo "<td>" . std_deviation($midExam) . "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>Final</td>";
+    echo "<td>" . ($finExam_total/count($finExam)) . "</td>";
+    echo "<td>" . $median_final . "</td>";
+    echo "<td>" . std_deviation($finExam) . "</td>";
+    echo "</tr>";
+
+    echo "</table>";
+    
     mysqli_close($conn);
 ?>
-
-  <script src="script/gradeFunctions.js"></script>
 </body>
 
 </html>
